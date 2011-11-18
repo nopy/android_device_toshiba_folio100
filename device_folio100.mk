9fa19408b11b064ed200dc286da3106654b0eb19
Nicolas Pinot <npinot@gmail.com>
2011-11-18 20:27:55 +0100

    Add tablet_core_hardware.xml to permission
    Change-Id: Ib1912a41c57b9700599ddc909dfde4d81491d3cc

--- device_folio100.mk
+++ device_folio100.mk
@@ -39,7 +39,9 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/base/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
+    frameworks/base/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+
 
 # Keychars
 # Keylayout
