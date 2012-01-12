cbcecb7feb0c77fbb0b90736e2381db9c100c9a4
Nicolas Pinot <npinot@gmail.com>
2012-01-12 18:14:35 +0100

    Use Galaxy Tab identity
    Change-Id: I717524030904f1c51a522c40197795414a4881a1

--- cm.mk
+++ cm.mk
@@ -1,5 +1,6 @@
 $(call inherit-product, device/toshiba/folio100/full_folio100.mk)
 
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
@@ -9,7 +10,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 #
 # Setup device specific product configuration.
 #
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-P7510 BUILD_ID=HTJ85B BUILD_FINGERPRINT=samsung/GT-P7510/GT-P7510:3.2/HTJ85B/UEKMM:user/release-keys PRIVATE_BUILD_DESC="GT-P7510-user 3.2 HTJ85B UEKMM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_MODEL=GT-P7510 PRODUCT_BRAND=samsung PRODUCT_MANUFACTURER=samsung PRODUCT_NAME=GT-P7510 TARGET_DEVICE=GT-P7510 BUILD_ID=HTJ85B BUILD_FINGERPRINT=samsung/GT-P7510/GT-P7510:3.2/HTJ85B/UEKMM:user/release-keys PRIVATE_BUILD_DESC="GT-P7510-user 3.2 HTJ85B UEKMM release-keys"
 
 PRODUCT_NAME := cm_folio100
 PRODUCT_DEVICE := folio100
